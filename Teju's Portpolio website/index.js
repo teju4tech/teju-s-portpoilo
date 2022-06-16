2ffba31ae2aafeeacc6f86a2a2f3035d66e6a19b
teju4tech <99065007+teju4tech@users.noreply.github.com>
2022-06-17 01:43:12 +0530

    Add files via upload
    hello

---
+++ Teju's Portpolio website/index.js
@@ -0,0 +1,15 @@
+alert ("Do not press ok. if you press ok.... your device will be Haked...");
+console.log("Hello gyas .. How are you ?")
+console.count(0)
+var a = 34;
+var b = 56;
+console.log(a+b);
+console.error("404 not found")
+
+let arr = [1,2,3,4,5,6,7,8]
+let j = 0;
+
+while (j<=arr.length){
+    console.log(j); 
+    j++;
+}
